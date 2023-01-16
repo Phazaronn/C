7db560388231e2b2e2dd407c9a4ebb3f28bdc3f0
Phazaronn <116441708+Phazaronn@users.noreply.github.com>
2023-01-17 01:28:46 +0300

    Add files via upload

---
+++ isim şehir.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char city[20];
+    printf("Enter a city name: ");
+    scanf("%s", city);
+    printf("The city you entered is %s\n", city);
+    return 0;
+}
+
